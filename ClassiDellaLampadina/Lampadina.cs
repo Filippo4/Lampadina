da4eb7ed48d57e37b0633b46125172d3c25628fc
Filippo <Dell@DESKTOP-2751OC0>
2020-10-25 15:21:41 +0100

    Aggiungere i file di progetto.

---
+++ ClassiDellaLampadina/Lampadina.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ClassiDellaLampadina
+{
+    public class Lampadina
+    {
+        public const int ACCESO = 0;
+        public const int SPENTO = 1;
+        public const int ROTTO = -1;
+        private const int MAXCLICK = 50;
+        private int stato = SPENTO;
+        private int cliclAttuali = 0;
+        public Lampadina()
+        {
+
+        }
+        public int Click()
+        {
+            if (stato != ROTTO)
+            {
+                cliclAttuali++;
+                if (cliclAttuali >= MAXCLICK)
+                {
+                    return stato = ROTTO;
+                }
+                else
+                {
+                    stato = 1 - stato;
+                }
+            }
+            return stato;
+        }
+        public string StatoAttuale()
+        {
+            
+            if (stato == SPENTO)
+                return "la lampadina è spenta!";
+            else if (stato == ACCESO)
+                return "la lampadina è accesa";
+            else
+                return "la lampadina è rotta";
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿using ClassiDellaLampadina;
+using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Lampadina l = new Lampadina();
+            bool fineCiclo = false;
+            do
+            {
+                Console.WriteLine("Che operazione vuoi fare? (Click/Descrizione/Termina)");
+                string risposta = Console.ReadLine();
+                if (risposta == "Click")
+                {
+                    l.Click();
+                    Console.WriteLine("Cliccato");
+                }
+                if (risposta == "Descrizione")
+                {
+                    Console.WriteLine(l.StatoAttuale());
+                }
+                if (risposta == "Termina")
+                {
+                    fineCiclo = true;
+                    Console.WriteLine("Premi invio per terminare");
+                }
+            } while (fineCiclo == false);
+            Console.ReadLine();
+        
+    }
+    }
+}
